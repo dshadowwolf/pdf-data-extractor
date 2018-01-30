a98835c3c67992cde4e37274ecf7528613baa6ce
Daniel Hazelton <dshadowwolf@gmail.com>
2018-01-29 19:05:38 -0500

    finish proof of concept for cleaning up the deskewed image and getting a final possible setup for the grid

--- edge_detect.py
+++ edge_detect.py
@@ -1,7 +1,11 @@
+#!/bin/env python
 import numpy as np
 import cv2
 from PIL import Image
+import math
 
+# load the original deskewed image, convert to greyscale
+# and run the edge detection
 im1 = cv2.imread('out/pdfimage_0000.jpg')
 img1gray = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
 edges1 = cv2.Canny(img1gray,50,150,apertureSize = 3)
@@ -10,9 +14,9 @@ edges1 = cv2.Canny(img1gray,50,150,apertureSize = 3)
 minLineLength=im1.shape[1]-600
 lines = cv2.HoughLinesP(image=edges1, rho=0.125, theta=np.pi/180, threshold=5, lines=np.array([]), minLineLength=minLineLength, maxLineGap=20)
 
-print lines
+# find the minima and maxima of the outermost lines, which represent the
+# black areas added during the deskew
 points = [im1.shape[1], im1.shape[0], 0, 0]
-
 for _line in lines:
         for segment in _line:
                 dir_x = segment[2] - segment[0]
@@ -29,24 +33,52 @@ for _line in lines:
                 points[2] = max( points[2], dir_x )
                 points[3] = max( points[3], im1.shape[0] - dir_y )
 
+# the hough permutation we did can miss some pixels, so pull the minima and maxima in
+# decreasing the final image by 4 pixels
+points[0] += 2
+points[1] += 2
+points[2] -= 2
+points[3] -= 2
+
+# actually do the crop
 im3 = Image.open("out/pdfimage_0000.jpg")
 im4 = im3.crop( (points[0], points[1], points[2], points[3]) )
 im4.save("out/pdfimage_0000_autocropped.jpg")
+
+# read the cropped image, convert to grayscale and edge-detect
 im2 = cv2.imread('out/pdfimage_0000_autocropped.jpg')
 img2gray = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
 edges2 = cv2.Canny(img2gray,50,150,apertureSize = 3)
 
-lines = cv2.HoughLinesP(image=edges2, rho=0.02, theta=np.pi/180, threshold=10, lines=np.array([]), minLineLength=75, maxLineGap=10)
+# line segment detector - don't need more than just the "LSD_REFINE_ADV"
+lsd = cv2.createLineSegmentDetector(_refine = cv2.LSD_REFINE_ADV )
+lines,width,prec,nfa = lsd.detect( edges2 )
 
+# prep for finding the forms outermost corners
 a,b,c = lines.shape
-im5 = np.zeros((im2.shape[0],im2.shape[1],3), np.uint8)
+points2 = [ im5.shape[1], im5.shape[0], 0, 0 ]
+
 for i in xrange(a):
         for line_ in lines[i]:
-                cv2.line( im5, (line_[0], line_[1]), (line_[2], line_[3]), (255,255,255), 2, cv2.LINE_AA )
+                # yes, its messy but its needed. Should probably offload the min/max bits to a helper in
+                # later versions
+                points2[0] = min( min( points2[0], int(math.floor(line_[0])) ), int(math.floor(line_[2])) )
+                points2[1] = min( min( points2[1], int(math.floor(line_[1])) ), int(math.floor(line_[3])) )
+                points2[2] = max( max( points2[2], int(math.floor(line_[0])) ), int(math.floor(line_[2])) )
+                points2[3] = max( max( points2[3], int(math.floor(line_[1])) ), int(math.floor(line_[3])) )
+
+# give a bit of extra room
+# expand out by 2 pixels top and bottom to give some extra room for stuff that might be on the edge of the document
+points2[0] -= 2
+points2[1] -= 2
+points2[2] += 2
+points2[3] += 2
+
+# draw in the box that we're going to do a final crop and/or use as the minima/maxima for the extraction
+im5 = im2.copy()
+cv2.line( im5, (points2[0], points2[1]), (points2[0], points2[3]), (0,0,255), 1, cv2.LINE_AA )
+cv2.line( im5, (points2[0], points2[3]), (points2[2], points2[3]), (0,0,255), 1, cv2.LINE_AA )
+cv2.line( im5, (points2[2], points2[3]), (points2[2], points2[1]), (0,0,255), 1, cv2.LINE_AA )
+cv2.line( im5, (points2[2], points2[1]), (points2[0], points2[1]), (0,0,255), 1, cv2.LINE_AA )
                 
-cv2.imwrite("out/pdfimage_0000_edge_detect.jpg", edges2)
-cv2.imwrite("out/pdfimage_0000_cropped_marked.jpg", im5)
-cv2.imshow('edges', edges2)
-cv2.imshow('result', im5)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite("out/pdfimage_0000_boxed.jpg", im5)
